3c1aaa8a7c7f55ec81283b7d725f833decce566e
Tyler Stuver <tyler@tylerstuver.dev>
2022-01-16 22:20:08 -0700

    added 11ty passthrough for assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
 module.exports = function(eleventyConfig) {
+    
+    eleventyConfig.addPassthroughCopy('src/assets')
+
+    // Return your Object options:
     return {
-        dir: {
-            input: "src",
-            output: "dist"
-        }
+      dir: {
+        input: "src",
+        output: "dist"
+      }
     }
-};
+  };
