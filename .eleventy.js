aa1fef90cfb6a9ea683e67b22e84668191e9532e
Tyler Stuver <tyler@tylerstuver.dev>
2022-02-02 15:53:22 -0700

    Add favicons

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function(eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy('src/assets')
-
+    eleventyConfig.addPassthroughCopy({"src/favicon":"/"})
     // Return your Object options:
     return {
       dir: {
